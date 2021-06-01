eac9958a093076b7ac7e54a863093d144c2946d8
Antoni Stevent <itzmjauz@gmail.com>
2021-06-02 00:49:44 +0200

    add basic args for java

--- sudoku-solve.java
+++ sudoku-solve.java
@@ -1,4 +1,12 @@
 class sudokuSolve  {
+    int WIDTH = 9;
+    int HEIGHT = 9;
+    int SCREEN_WIDTH = HEIGHT * 3 + 4;
+    int ROUND_LIMIT = 1000;
+
+    int start_world[][] = new int[WIDTH][HEIGHT];
+    boolean possibilities[][][] = new boolean[WIDTH][HEIGHT][WIDTH + 1]; //width + 1 so we don't have to use offsets 
+
     public static void main(String[] args) {
         System.out.println("Hello, World!"); 
     }
